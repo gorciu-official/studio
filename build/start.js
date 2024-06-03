5f60e2a474ab4a6ebd3120ac9fe0269a09e4b43f
Gorciu <gorciuyt@gmail.com>
2024-06-03 12:19:37 +0200

    Updated prestart script

--- build/start.js
+++ build/start.js
@@ -4,3 +4,14 @@
  *                       (C) 2024 Gorciu
  * ==================================================================
 **/
+
+let err = false;
+
+if (parseInt(/^(\d+)\.(\d+)\.(\d+)/.exec(process.versions.node)[1]) < 21) {
+    err = true;
+}
+
+if (err) {
+    console.error("Don't play with the code! Remove errors, it isn't very hard!");
+    process.exit(1);
+}
