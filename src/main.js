6e34d21f015792da540030a085e59e0954f27fe6
Gorciu <gorciuyt@gmail.com>
2024-06-06 13:50:39 +0200

    Modified the file to open Gorciu STudio properly

--- src/main.js
+++ src/main.js
@@ -8,3 +8,36 @@
  * Contributing instructions:   CONTRIBUTING.MD
  * License:                     LICENSE.MD
 */
+
+// Get required items from Electron library
+const {app, BrowserWindow} = require('electron');
+
+// Create a function to create a window
+function createWindow() {
+    // Create a browser window instance
+    var bw = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        frame: false,
+        webPreferences: {
+            devTools: false,
+            nodeIntegration: true
+        }
+    });
+
+    bw.loadFile('./src/editor/editor.html');
+
+    // Returns instance
+    return bw;
+}
+
+// The app is ready to display window
+app.whenReady(() => {
+    // Create window
+    createWindow();
+
+    // Close app when all windows were closed
+    app.on('window-all-closed', () => {
+        app.quit();
+    })
+})
