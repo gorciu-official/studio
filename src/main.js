c6abf158e99c9178961e7ff6d163de125b0317b0
Gorciu <gorciuyt@gmail.com>
2024-06-06 13:40:03 +0200

    Created main file to run Electron app

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * ======================================================================
+ *                           Gorciu Studio
+ *                          (C) 2024 Gorciu
+ * ======================================================================
+ * 
+ * Read before doing anything:  README.MD
+ * Contributing instructions:   CONTRIBUTING.MD
+ * License:                     LICENSE.MD
+*/
