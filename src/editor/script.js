3270419385332c4d3665550644f7414ad0bb1ae2
Gorciu <gorciuyt@gmail.com>
2024-06-06 14:04:55 +0200

    Created an web app

---
+++ src/editor/script.js
@@ -0,0 +1,25 @@
+/**
+ * ======================================================================
+ *                           Gorciu Studio
+ *                          (C) 2024 Gorciu
+ * ======================================================================
+ * 
+ * Read before doing anything:  README.MD
+ * Contributing instructions:   CONTRIBUTING.MD
+ * License:                     LICENSE.MD
+*/
+
+// Say that the script has been loaded
+console.log("Loaded `script.js`");
+
+// Gets an element
+function get(pattern) {
+    return document.querySelector(pattern);
+}
+
+// Changes title
+function changeTitle(newTitle) {
+    newTitle = newTitle + ' - Gorciu Studio';
+    get('.title-bar .title').textContent = newTitle;
+    get('title').textContent = newTitle;
+}
